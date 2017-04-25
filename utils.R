779653caae28447b88bf6a968596d007c6dbeb5c
Mike Korcynski <mkorcy@gmail.com>
2017-04-25 18:37:14 -0400

    adds method for combining strings

--- utils.R
+++ utils.R
@@ -1,8 +1,8 @@
 # http://stackoverflow.com/questions/2261079/how-to-trim-leading-and-trailing-whitespace-in-r
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
-
+'%&%' <- function(x, y) paste0(x,y)
 log_error_to_console <- function(cond, id, display_name, last_name, first_name) {
-  print(error)
+  print("error")
   print(cond)
   print(id)
   print(display_name)
